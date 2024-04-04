66cb49608162b03b66e56096c0bfb2dbf8df984b
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-04-04 15:20:34 +0700

    membuat step 5

--- AlgoritmaSearch.cpp
+++ AlgoritmaSearch.cpp
@@ -47,10 +47,20 @@ void input() {
                 break;
             }
             i++; // step 4
-        }
+              if (i == n) // step 5
+            cout << "\n" << item << " not found in the array\n";
+        cout << "\nNumber of comparisons: " << ctr << endl;
+
+        cout << "\nContinue search (y/n): ";
+        cin >> ch;
+
+    } while ((ch == 'y') || (ch == 'Y'));
+    }
 
 
 }
 
+
 }
+
 
