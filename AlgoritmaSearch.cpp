4fb5012810b863d359343b3a2e1e4a3816fffeda
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-04-04 15:17:02 +0700

    membuat sistem accept

--- AlgoritmaSearch.cpp
+++ AlgoritmaSearch.cpp
@@ -16,4 +16,11 @@ void input() {
          cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
     }
 
+// Accept array elements
+    cout << "\n-------------------\n";
+    cout << "Enter array elements \n";
+    cout << "---------------------\n";
+    for (i = 0; i < n; i++)
+
+
 }
