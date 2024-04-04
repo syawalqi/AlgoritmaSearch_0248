e82126c3b3670d48877ae948e6d24880ed3371a6
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-04-04 15:19:40 +0700

    meneyelesaikan system do while

--- AlgoritmaSearch.cpp
+++ AlgoritmaSearch.cpp
@@ -36,9 +36,21 @@ void input() {
         // Accept the number to be searched
         cout << "\nEnter the element you want to search: "; // step 1
         cin >> item;
+   ctr = 0;
+        i = 0; // step 2
+        while (i < n) // step 3
+        {
+            ctr++;
+            if (arr[i] == item)
+            {
+                cout << "\n" << item << " found at position " << (i + 1) << endl; // step 6
+                break;
+            }
+            i++; // step 4
+        }
 
 
 }
 
-
+}
 
